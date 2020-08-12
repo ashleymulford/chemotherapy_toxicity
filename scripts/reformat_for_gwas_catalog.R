6f19a8de051666819da98775743b8c04ae1f192d
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-08-12 17:10:31 -0400

    Update reformat_for_gwas_catalog.R

--- scripts/reformat_for_gwas_catalog.R
+++ scripts/reformat_for_gwas_catalog.R
@@ -4,18 +4,20 @@ library(data.table)
 #Paste function
 "%&%" = function(a,b) paste(a,b,sep="")
 
-#Create list of drugs for file input
+pop_list <- c("YRI", "CEU", "ASN", "ALL")
 drug_list <- c("arac", "capecitabine", "carboplatin", "cisplatin", "daunorubicin", "etoposide", "paclitaxel", "pemetrexed")
 
-for (drug in drug_list){
-  GWAS_CEU <- fread("sumstats/CEU_GWAS_results/CEU_GWAS_"%&% drug %&%".assoc.txt")
-  #remove columns
-  GWAS_CEU$n_miss <- NULL
-  GWAS_CEU$p_lrt <- NULL
-  GWAS_CEU$l_mle <- NULL
-  GWAS_CEU$l_remle <- NULL
-  GWAS_CEU$p_score <- NULL
-  #change column names
-  colnames(GWAS_CEU) <- c("chromosome", "variant_ID", "base_pair_location", "effect_allele", "other_allele", "effect_allele_frequency", "beta", "standard_error", "p_value")
-  fwrite(GWAS_CEU, "sumstats/CEU_GWAS_results/CEU_"%&% drug %&%".assoc.tsv", quote = F, sep = "\t", col.names = T)
+for (pop in pop_list) {
+  for (drug in drug_list){
+    GWAS <- fread("sumstats/"%&% pop %&%"_GWAS_results/"%&% pop %&%"_GWAS_"%&% drug %&%".assoc.txt")
+    #remove columns
+    GWAS$n_miss <- NULL
+    GWAS$p_lrt <- NULL
+    GWAS$l_mle <- NULL
+    GWAS$l_remle <- NULL
+    GWAS$p_score <- NULL
+    #change column names
+    colnames(GWAS) <- c("chromosome", "variant_ID", "base_pair_location", "effect_allele", "other_allele", "effect_allele_frequency", "beta", "standard_error", "p_value")
+    fwrite(GWAS, "sumstats/"%&% pop %&%"_GWAS_results/"%&% pop %&%"_"%&% drug %&%".assoc.tsv", quote = F, sep = "\t", col.names = T)
+  }
 }
